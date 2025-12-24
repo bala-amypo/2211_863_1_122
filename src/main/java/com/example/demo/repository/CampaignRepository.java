57900e78bc1686d96855ccfff0096128ff5020a3
bala-amypo <bala@amypo.in>
2025-12-24 05:48:42 +0000

    User commit

--- src/main/java/com/example/demo/repository/CampaignRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Campaign;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CampaignRepository extends JpaRepository<Campaign, Long> {
-}
--- src/main/java/com/example/demo/repository/RoiReportRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.RoiReport;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoiReportRepository extends JpaRepository<RoiReport, Long> {
-}
