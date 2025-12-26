0ab99c2c1fefc991fe6c322a346270a92c184378
bala-amypo <bala@amypo.in>
2025-12-26 04:02:44 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,22 +1,20 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.*;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-            .info(new Info().title("Influencer ROI API").version("1.0"))
-            .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-            .components(new io.swagger.v3.oas.models.Components()
-                .addSecuritySchemes("bearerAuth", new SecurityScheme()
-                    .type(SecurityScheme.Type.HTTP)
-                    .scheme("bearer")
-                    .bearerFormat("JWT")));
-    }
+                // You need to change the port as per your server
+                .servers(List.of(
+                        new Server().url("https://9102.32procr.amypo.ai/")
+                ));
+        }
 }
