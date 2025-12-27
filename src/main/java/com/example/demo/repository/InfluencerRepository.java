1ebdec87a0e5b41b917da61bed26fb4daaf639aa
bala-amypo <bala@amypo.in>
2025-12-27 04:36:02 +0000

    User commit

--- src/main/java/com/example/demo/repository/InfluencerRepository.java
+++ src/main/java/com/example/demo/repository/InfluencerRepository.java
@@ -2,8 +2,10 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Influencer;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
 public interface InfluencerRepository extends JpaRepository<Influencer, Long> {
-    Optional<Influencer> findBySocialHandle(String socialHandle);
+
+    Optional<Influencer> findBySocialHandle(String handle);
 }
--- src/main/java/com/example/demo/repository/SaleTransactionRepository.java
+++ src/main/java/com/example/demo/repository/SaleTransactionRepository.java
@@ -2,10 +2,14 @@ package com.example.demo.repository;
 
 import com.example.demo.model.SaleTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 
 public interface SaleTransactionRepository extends JpaRepository<SaleTransaction, Long> {
+
     List<SaleTransaction> findByDiscountCode_Id(Long codeId);
+
     List<SaleTransaction> findByDiscountCode_Influencer_Id(Long influencerId);
+
     List<SaleTransaction> findByDiscountCode_Campaign_Id(Long campaignId);
 }
